0fea99d44d2272885a770b8352198f1d659e77c9
mohamed <mohamedkazafy78@gmail.com>
2024-01-19 21:19:05 +0200

    sec-commit

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -1,13 +1,17 @@
-let check = document.getElementById("mail")
-let btn = document.getElementById("btn")
-let err = document.getElementById("error")
-btn.addEventListener('click',function(){
-    if(check.value = "" || !check.value.includes("@"||".com")){
-        err.innerText = "enter vaild email"
-        err.style.color = "red"
-    }
-})
-function toggleNav() {
-    var navbar = document.getElementById("navbar");
-    navbar.classList.toggle("show");
-}
+let check = document.getElementById("mail");
+let btn = document.getElementById("btn");
+let err = document.getElementById("error");
+
+btn.addEventListener('click', function() {
+  
+  if (check.value === "" || !check.value.includes("@") || !check.value.includes(".com")) {
+    err.innerText = "Enter a valid email";
+    err.style.color = "red";
+    err.style.opacity = "1";
+  } else {
+    err.innerText = "Done";
+    err.style.color = "green";
+    err.style.opacity = "1";
+  }
+});
+
