ec9c7269f851a700b66b9bf1fdbafffd9ada3903
mohamed <mohamedkazafy78@gmail.com>
2024-01-19 21:50:08 +0200

    last commit

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -7,10 +7,14 @@ btn.addEventListener('click', function() {
   if (check.value === "" || !check.value.includes("@") || !check.value.includes(".com")) {
     err.innerText = "Enter a valid email";
     err.style.color = "red";
+    check.style.color = "red"
+    check.style.border = "1px solid red"
     err.style.opacity = "1";
   } else {
     err.innerText = "Done";
     err.style.color = "green";
+    check.style.color = "black"
+    check.style.border = "1px solid black"
     err.style.opacity = "1";
   }
 });
